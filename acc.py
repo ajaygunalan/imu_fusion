df47078f1dcfc49fa3b2bc33c3add33db65d1ffd
ajay <ajaygunalan1995@gmail.com>
2019-05-13 15:45:25 +0530

    rough psuedo code done

--- acc.py
+++ acc.py
@@ -18,34 +18,36 @@ while 1:
 		data_ls = message_str.split(",")
 		data_ls = [float(i) for i in data_ls]
 		# print data_ls
-		var_ls = ['time_stamp','accl_id','accl_x','accl_y','accl_z']
+		var_ls = ['time_stamp','accl_id','accl_x','accl_y','accl_z', 'magn_id', 'magn_x', 'magn_y', 'magn_z', 'gyro_id', 'gyro_x', 'gryo_y', 'gyro_z']
 		data = dict(zip(var_ls, data_ls))
+        # acc = m/s^2;  gyro=rad/sec;  magno=microTelsa
 
 		# Combine Accelerometer(m/s^2) to get angle values
 		
 		pitch_x_accel = 180 * math.atan2(data['accl_x'], math.sqrt((data['accl_y']*data['accl_y']) + (data['accl_z']*data['accl_z'])))/math.pi;
 		roll_y_accel = 180 * math.atan2(data['accl_y'], math.sqrt((data['accl_x']*data['accl_x']) + (data['accl_z']*data['accl_z'])))/math.pi;
-		print "pitch_x_accel : ", pitch_x_accel, "roll_y_accel : ", roll_y_accel
+		print "pitch_x_accel :", pitch_x_accel, "roll_y_accel :", roll_y_accel
 		
 
+		# Calculate YAW from magnetometer formual taken from http://students.iitk.ac.in/roboclub/2017/12/21/Beginners-Guide-to-IMU.html
 
-         
-        # print data_ls[2]
-        # , accl_z, accl_z
-        # print (data_ls[2])
-        # , type(accl_z), type(accl_z))
+		mag_x_update = data['magn_x']*cos(pitch_x_accel) + data['magn_y']*sin(roll_y_accel)*sin(pitch_x_accel) + data['magn_z']*cos(roll_y_accel)*sin(pitch_x_accel)
+		mag_y_update = data['magn_y']*cos(roll_y_accel) - data['magn_z']*sin(roll_y_accel)
+		yaw_z_magn = 180 * atan2(-mag_y_update,mag_x_update)/M_PI;
+
+		# Calculate the loop time from the timestamp
 
 
+		# Calculate the angles from the gyro
+		# gyroXangle+=rate_gyr_x*DT;
+		# gyroYangle+=rate_gyr_y*DT;
+		# gyroZangle+=rate_gyr_z*DT;
 
-		#data_ls[5]
-		# gryo_x = data_ls[6] #in rad/sec 
-		# gryo_y = data_ls[7] #in rad/sec
-		# gyro_z = data_ls[8] #in rad/sec
-
-		# #data_ls[9]
-		# magn_x = data_ls[10] #in micro-Telsa
-		# magn_y = data_ls[11] #in micro-Telsa
-		# magn_z = data_ls[12] #in micro-Telsa
+		# #Fusion to get Pitch 
+		# CFangleX=AA*(CFangleX+rate_gyr_x*DT) +(1 - AA) * AccXangle;
+		# CFangleY=AA*(CFangleY+rate_gyr_y*DT) +(1 - AA) * AccYangle;
+		# CFangleY=AA*(CFangleY+rate_gyr_y*DT) +(1 - AA) * yaw;
+      
 
 	except (KeyboardInterrupt, SystemExit):
 		raise
@@ -59,10 +61,6 @@ while 1:
 # pitch = 180 * atan2(accelX, sqrt(accelY*accelY + accelZ*accelZ))/PI;
 # roll = 180 * atan2(accelY, sqrt(accelX*accelX + accelZ*accelZ))/PI;
 
-# # Calculate the angles from the gyro
-# gyroXangle+=rate_gyr_x*DT;
-# gyroYangle+=rate_gyr_y*DT;
-# gyroZangle+=rate_gyr_z*DT;
 
 
 # #Calculate YAW from magnetometer
